2cabdcfd8cdc74eb92b84c97dc21a646b7c2ed40
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-03-18 16:08:15 -0300

    styles fixed

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function Link({ handleSubmit, link, setLink }: Props) {
   return (
     <div className="flex flex-col">
-      <div className="bg-primary-dark-violet bg-[url('./assets/bg-shorten-desktop.svg')] bg-cover py-8 px-10 rounded-md relative overflow-hidden mx-6 md:mx-48 translate-y-[50%]">
+      <div className="bg-primary-dark-violet bg-[url('./assets/bg-shorten-desktop.svg')] bg-cover py-4 md:py-8 px-5 md:px-10 rounded-md relative overflow-hidden mx-6 md:mx-48 translate-y-[50%]">
         <form
           className="flex flex-col lg-flex-row gap-3"
           onSubmit={(e) => handleSubmit(e)}
